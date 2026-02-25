9e05380c08dc8ee5ada9b8990d74b9a17351de2d
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-02-25 15:00:01 -0300

    .

--- cap5/ex_5_7.py
+++ cap5/ex_5_7.py
@@ -5,3 +5,29 @@
 # • Escreva cinco instruções if. Cada instrução deve verificar se uma determinada
 # fruta está em sua lista. Se estiver, o bloco if deverá exibir uma frase, por
 # exemplo, Você realmente gosta de bananas!
+
+favorite_fruits = ["banana", "melancia", "abacaxi"]
+
+if "abacaxi" in favorite_fruits:
+    print("\nVocê realmente gosta de abacaxi\n")
+
+if "abacaxi" not in favorite_fruits:
+    print("\nVocê realmente NÃO gosta de abacaxi\n")
+
+if "mamão" in favorite_fruits:
+    print("\nVocê realmente gosta de mamão!\n")
+
+if "mamão" not in favorite_fruits:
+    print("\nVocê realmente NÃO gosta de mamão!\n")
+
+if "melancia" in favorite_fruits:
+    print("\nVocê realmente gosta de melancia\n")
+
+if "melancia" not in favorite_fruits:
+    print("\nVocê realmente NÃO gosta de melancia\n")
+
+if "melão" in favorite_fruits:
+    print("\nVocê realmente gosta de melão!\n")
+
+if "melão" not in favorite_fruits:
+    print("\nVocê realmente NÃO gosta de melão!\n")
