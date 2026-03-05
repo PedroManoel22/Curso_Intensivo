9d308a03709eacd8fa5571b210d6ad97ed1b8360
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-05 11:49:53 -0300

    .

---
+++ cap8/ex_8_11.py
@@ -0,0 +1,6 @@
+# 8.11 – Mágicos inalterados: Comece com o trabalho feito no Exercício 8.10.
+# Chame a função make_great() com uma cópia da lista de nomes de mágicos.
+# Como a lista original não será alterada, devolva a nova lista e armazene-a em uma
+# lista separada. Chame show_magicians() com cada lista para mostrar que você
+# tem uma lista de nomes originais e uma lista com a expressão o Grande
+# adicionada ao nome de cada mágico
