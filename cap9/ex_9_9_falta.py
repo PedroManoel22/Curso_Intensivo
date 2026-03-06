61255404874a4671f41c35b71cb31043f8a3b178
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-06 16:32:28 -0300

    Update ex9_9.py

---
+++ cap9/ex_9_9_falta.py
@@ -0,0 +1,45 @@
+# 9.9 – Upgrade de bateria: Use a última versão de electric_car.py desta seção.
+# Acrescente um método chamado upgrade_battery() na classe Battery. Esse
+# método deve verificar a capacidade da bateria e defini-la com 85 se o valor for
+# diferente. Crie um carro elétrico com uma capacidade de bateria default, chame
+# get_range() uma vez e, em seguida, chame get_range() uma segunda vez após
+# fazer um upgrade da bateria. Você deverá ver um aumento na distância que o
+# carro é capaz de percorrer
+
+
+class Car:
+    """Uma tentativa simples de representar um carro."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + " " + self.make + " " + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Representa aspectos específicos de veículos elétricos."""
+
+    def __init__(self, make, model, year):
+        """Inicializa os atributos da classe-pai."""
+        super().__init__(make, model, year)
+
+
+my_tesla = ElectricCar("tesla", "model s", 2016)
+print(my_tesla.get_descriptive_name())
