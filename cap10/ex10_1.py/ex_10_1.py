f7aaffea4a1e90644d67a0c030da2edb8dabd1dd
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-12 10:01:33 -0300

    .

---
+++ cap10/ex10_1.py/ex_10_1.py
@@ -0,0 +1,23 @@
+# 10.1 – Aprendendo Python: Abra um arquivo em branco em seu editor de texto e
+# escreva algumas linhas que sintetizem o que você aprendeu sobre Python até
+# agora. Comece cada linha com a expressão Em Python podemos.... Salve o
+# arquivo como learning_python.txt no mesmo diretório em que estão seus exercícios
+# deste capítulo. Escreva um programa que leia o arquivo e mostre o que você
+# escreveu, três vezes. Exiba o conteúdo uma vez lendo o arquivo todo, uma vez
+# percorrendo o objeto arquivo com um laço e outra armazenando as linhas em uma
+# lista e então trabalhando com ela fora do bloco with.
+
+
+# lendo o arquivo todo
+adress = "Curso_Intensivo/cap10/ex10_1.py/learning_python.txt"
+print()
+with open(adress, encoding="utf-8") as file_object:
+    file = file_object.read()
+    print(file)
+
+print()
+
+# percorrendo o objeto arquivo com um laço
+
+
+# armazenando as linhas em uma lista e então trabalhando com ela fora do bloco with
