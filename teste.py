162411ca7093a3d0f241675e530e1b322e4ca95b
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-18 17:45:42 -0300

    Implementando Type Hints

--- teste.py
+++ teste.py
@@ -3,10 +3,9 @@ print(id(dimensions))
 print("Original dimensions:")
 for dimension in dimensions:
     print(dimension)
- 
+
 dimensions = (400, 100)
 print(id(dimensions))
 print("\nModified dimensions:")
 for dimension in dimensions:
     print(dimension)
-
