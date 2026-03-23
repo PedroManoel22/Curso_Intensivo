8bdb4857441f9300989031816064a1d696516ab8
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-23 18:06:01 -0300

    Update name_function.py

--- cap10/examples/name_function.py
+++ cap10/examples/name_function.py
@@ -1,4 +1,4 @@
-def get_formatted_name(first: str, last: str):
+def get_formatted_name(first: str, middle: str, last: str):
     """Gera um nome completo formatado de modo elegante"""
-    full_name = first + " " + last
+    full_name = first + " " + middle + " " + last
     return full_name.title()
