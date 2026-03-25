a370f544da36ab1380e868f31700fcac1480019e
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-25 17:56:32 -0300

    del

--- cap10/examples/name_function.py
+++
@@ -1,4 +0,0 @@
-def get_formatted_name(first: str, middle: str, last: str):
-    """Gera um nome completo formatado de modo elegante"""
-    full_name = first + " " + middle + " " + last
-    return full_name.title()
