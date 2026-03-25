23c5fe9adf77e742312e896f430476095ae2b743
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-25 18:37:02 -0300

    Create alien_invasion.py

---
+++ alien_ivasion/alien_invasion.py
@@ -0,0 +1,37 @@
+import sys
+
+import pygame
+
+
+def run_game():
+    # Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))  # Janela inteira do jogo
+    pygame.display.set_caption("Alien Invasion")
+
+    # Define a cor de fundo
+
+    #            R    G    B
+    bg_color = (230, 230, 230)  # Branco
+    bg_color = (255, 0, 0)  # Vermelho
+    bg_color = (0, 255, 0)  # Verde
+    bg_color = (0, 0, 255)  # Azul
+
+    # Inicia o laço principal do jogo
+    while True:
+        # Observa eventos de teclado e mouse
+        for event in (
+            pygame.event.get()
+        ):  # pygame.event.get() -> acessarv todos os eventos detectados
+            if (
+                event.type == pygame.QUIT
+            ):  # quando o evento for == ao usuário cliclar no botão de fechamento
+                sys.exit()
+
+        # Redesenha a tela a cada passagem pelo laço
+        screen.fill(bg_color)
+        # Deixa a tela mais recente visível
+        pygame.display.flip()
+
+
+run_game()
