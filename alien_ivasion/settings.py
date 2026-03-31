7c65295a2b2e88c444570e2df6cdc7353a53e8b5
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-31 10:31:42 -0300

    Update settings.py

--- alien_ivasion/settings.py
+++ alien_ivasion/settings.py
@@ -8,4 +8,11 @@ class Settings:
 
         self.screen_widht = 1200
         self.screen_height = 800
+
+        # Define a cor de fundo
         self.bg_color = (0, 0, 80)
+        #            R    G    B
+        # bg_color = (230, 230, 230)  # Branco
+        # bg_color = (255, 0, 0)  # Vermelho
+        # bg_color = (0, 255, 0)  # Verde
+        # bg_color = (0, 0, 255)  # Azul
