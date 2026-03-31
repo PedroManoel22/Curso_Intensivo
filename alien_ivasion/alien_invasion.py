7f7a06f411ad9f2fc76a78175080d54c2a78d391
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-31 10:31:03 -0300

    Update alien_invasion.py

--- alien_ivasion/alien_invasion.py
+++ alien_ivasion/alien_invasion.py
@@ -1,5 +1,4 @@
-import sys
-
+import game_functions as gf
 import pygame
 from settings import Settings
 from ship import Ship
@@ -11,36 +10,17 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_widht, ai_settings.screen_height)
-    )  # Janela inteira do jogo
+    )
+    # Janela inteira do jogo
     pygame.display.set_caption("Alien Invasion")
 
     # Cria uma espaçonave
     ship = Ship(screen)
 
-    # Define a cor de fundo
-
-    #            R    G    B
-    # bg_color = (230, 230, 230)  # Branco
-    # bg_color = (255, 0, 0)  # Vermelho
-    # bg_color = (0, 255, 0)  # Verde
-    # bg_color = (0, 0, 255)  # Azul
-
     # Inicia o laço principal do jogo
     while True:
-        # Observa eventos de teclado e mouse
-        for event in (
-            pygame.event.get()
-        ):  # pygame.event.get() -> acessarv todos os eventos detectados
-            if (
-                event.type == pygame.QUIT
-            ):  # quando o evento for == ao usuário cliclar no botão de fechamento
-                sys.exit()
-
-        # Redesenha a tela a cada passagem pelo laço
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        # Deixa a tela mais recente visível
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
