1ccfd8c8cf4f7a44a7f301fa4c5ab9151c54d5eb
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-02 16:36:47 -0300

    Create main.py

---
+++ cap12/ex12_4/main.py
@@ -0,0 +1,40 @@
+# 12.4 – Teclas: Em um arquivo Pygame, crie uma tela vazia. No laço de eventos,
+# exiba o atributo event.key sempre que o evento pygame.KEYDOWN for detectado.
+# Execute o programa e pressione várias teclas para ver como o Pygame responde.
+
+# 12.1 – Céu azul: Crie uma janela do Pygame com uma cor de fundo azul
+
+import sys
+
+import pygame
+
+
+def run_game(title: str = "Tela Azul"):
+    # Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+
+    screen_widht = 1200
+    screen_height = 800
+    bg_color = (0, 0, 80)
+
+    screen = pygame.display.set_mode((screen_widht, screen_height))
+
+    # Janela inteira do jogo
+    pygame.display.set_caption(title)
+
+    # Inicia o laço principal do jogo
+    while True:
+        screen.fill(bg_color)
+
+        for event in pygame.event.get():
+            # pygame.event.get() -> acessar todos os eventos detectados
+            if event.type == pygame.QUIT:
+                # quando o evento for == ao usuário cliclar no botão de fechamento
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                print(event.key)
+
+        pygame.display.flip()
+
+
+run_game()
