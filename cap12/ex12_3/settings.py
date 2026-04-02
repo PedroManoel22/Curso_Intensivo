360701d46a73a9ac31fd4ce2667b919f6e58ed74
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-02 16:36:18 -0300

    Create settings.py

---
+++ cap12/ex12_3/settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    def __init__(self) -> None:
+        # Configurações do tamanho da tela
+        self.screen_widht = 1200
+        self.screen_height = 800
+
+        # Configurações da cor de fundo da tela
+        self.bg_color = (0, 0, 80)
+
+        # Configurações da espaçonave
+        self.ship_speed_factor = 1
