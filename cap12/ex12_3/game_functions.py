9fc1b14cdf8e03bc1afcd69e5e835472a976c571
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-02 16:35:55 -0300

    Create game_functions.py

---
+++ cap12/ex12_3/game_functions.py
@@ -0,0 +1,65 @@
+import sys
+
+import pygame
+from settings import Settings
+from ship import Ship
+
+# Tipagem
+
+def check_keydown_events(event: pygame.event.Event, ship: Ship):
+    """Responde a pressionamento de tecla."""
+
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+    if event.key == pygame.K_UP:
+        ship.moving_up = True
+
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
+def check_keyup_events(event: pygame.event.Event, ship: Ship):
+    """Responde a solturas de tecla."""
+
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    
+    if event.key == pygame.K_UP:
+        ship.moving_up = False
+
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
+
+def check_events(ship: Ship) -> None:
+    """Responde a eventos de pressionamento de teclas e mouse"""
+
+    # Observa eventos de teclado e mouse
+    for event in pygame.event.get():
+        # pygame.event.get() -> acessar todos os eventos detectados
+        if event.type == pygame.QUIT:
+            # quando o evento for == ao usuário cliclar no botão de fechamento
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+            
+        elif event.type == pygame.KEYUP:
+               check_keyup_events(event, ship)
+            
+
+
+def update_screen(ai_settings: Settings, screen: pygame.Surface, ship: Ship) -> None:
+    """Atualiza as imagens na tela e alterna para a nova tela."""
+    # Redesenha a tela a cada passagem pelo laço
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Deixa a tela mais recente visível
+    pygame.display.flip()
