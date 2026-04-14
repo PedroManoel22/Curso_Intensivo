ef4022d855ef229eda0c5614ff53a4719d335c32
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-14 08:52:43 -0300

    Update game_fuction.py

--- alien_ivasion/game_functions.py
+++ alien_ivasion/game_functions.py
@@ -1,12 +1,24 @@
+from __future__ import annotations  # DEVE SER A LINHA 1
+
 import sys
+from typing import Any
 
 import pygame
+from bullet import Bullet
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 
 # Tipagem
 
-def check_keydown_events(event: pygame.event.Event, ship: Ship):
+
+def check_keydown_events(
+    event: pygame.event.Event,
+    ai_settings: Settings,
+    screen: pygame.Surface,
+    ship: Ship,
+    bullets: Group[Any],
+) -> None:
     """Responde a pressionamento de tecla."""
 
     if event.key == pygame.K_RIGHT:
@@ -16,6 +28,11 @@ def check_keydown_events(event: pygame.event.Event, ship: Ship):
     elif event.key == pygame.K_LEFT:
         # Move a espaçonave para a esquerda
         ship.moving_left = True
+
+    elif event.key == pygame.K_SPACE:
+        # Cria um novo projétil e o adiciona ao grupo de projéteis
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event: pygame.event.Event, ship: Ship):
@@ -28,7 +45,9 @@ def check_keyup_events(event: pygame.event.Event, ship: Ship):
         ship.moving_left = False
 
 
-def check_events(ship: Ship) -> None:
+def check_events(
+    ai_settings: Settings, screen: pygame.Surface, ship: Ship, bullets: Group[Any]
+) -> None:
     """Responde a eventos de pressionamento de teclas e mouse"""
 
     # Observa eventos de teclado e mouse
@@ -39,17 +58,22 @@ def check_events(ship: Ship) -> None:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
-            
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+
         elif event.type == pygame.KEYUP:
-               check_keyup_events(event, ship)
-            
+            check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings: Settings, screen: pygame.Surface, ship: Ship) -> None:
+def update_screen(
+    ai_settings: Settings, screen: pygame.Surface, ship: Ship, bullets: "Group[Any]"
+) -> None:
     """Atualiza as imagens na tela e alterna para a nova tela."""
     # Redesenha a tela a cada passagem pelo laço
     screen.fill(ai_settings.bg_color)
+
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
 
     # Deixa a tela mais recente visível
