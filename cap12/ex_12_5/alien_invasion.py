b274dfd9c7b0ec5e9e575e0ba8d85770776fa3e6
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-28 09:43:10 -0300

    Create cap12/ex_12_5/alien_invasion.py_

---
+++ cap12/ex_12_5/alien_invasion.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+import game_functions as gf
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    # Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_widht, ai_settings.screen_height)
+    )
+    # Janela inteira do jogo
+    pygame.display.set_caption("Alien Invasion")
+
+    # Cria uma espaçonave
+    ship = Ship(ai_settings, screen)
+
+    # Cria um grupo no qual serão armazenados os projéteis
+    bullets: Group[Any] = Group()
+
+    # Inicia o laço principal do jogo
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+
+run_game()
