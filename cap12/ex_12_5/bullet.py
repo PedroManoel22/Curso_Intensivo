77f7b3fb15687a7983419102f05038dfffc8e8ef
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-28 10:22:08 -0300

    Update cap12\ex_12_5\bullet.py

--- cap12/ex_12_5/bullet.py
+++ cap12/ex_12_5/bullet.py
@@ -23,24 +23,24 @@ class Bullet(Sprite):
         self.rect = pygame.Rect(
             0, 0, ai_settings.bullet_width, ai_settings.bullet_height
         )
-        self.rect.centerx = ship.rect.centerx
-        self.rect.top = ship.rect.top
+
+        self.rect.midleft = ship.rect.midleft
 
         # Armazena a posição do projétil como um valor decimal
-        self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
 
     def update(self):
-        """Move o projétil para cima na tela."""
+        """Move o projétil para a direita na tela."""
         # Atualiza a posição decimal do projétil
 
-        self.y -= self.speed_factor
+        self.x += self.speed_factor
 
         # Atualiza a posição de rect
 
-        self.rect.y = int(self.y)
+        self.rect.x = int(self.x)
 
     def draw_bullet(self):
         """Desenha o projétil na tela."""
