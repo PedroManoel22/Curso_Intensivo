66bca49b4c7307f74d063f7dd11fce429814d253
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-28 10:22:35 -0300

    Update cap12\ex_12_5\settings.py

--- cap12/ex_12_5/settings.py
+++ cap12/ex_12_5/settings.py
@@ -22,7 +22,7 @@ class Settings:
 
         # configurações dos projéteis
         self.bullet_speed_factor = 1
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_width = 15
+        self.bullet_height = 3
         self.bullet_color = 255, 165, 0
         self.bullets_allowed = 3
