bde8d83c82d7e5b65866319d899038d07beecc9d
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-28 10:21:13 -0300

    Update alien_ivasion\_functions.py

--- alien_ivasion/game_functions.py
+++ alien_ivasion/game_functions.py
@@ -29,6 +29,12 @@ def check_keydown_events(
         # Move a espaçonave para a esquerda
         ship.moving_left = True
 
+    if event.key == pygame.K_UP:
+        ship.moving_up = True
+
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
 
@@ -41,6 +47,12 @@ def check_keyup_events(event: pygame.event.Event, ship: Ship):
 
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+
+    if event.key == pygame.K_UP:
+        ship.moving_up = False
+
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
 
 
 def check_events(
