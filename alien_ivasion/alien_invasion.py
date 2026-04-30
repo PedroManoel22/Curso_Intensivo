9e08c9bac7bb0991a1f6650fb6644e7dbe52b557
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-30 09:31:02 -0300

    Instanciando a classe Alien

--- alien_ivasion/alien_invasion.py
+++ alien_ivasion/alien_invasion.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import game_functions as gf
 import pygame
+from alien import Alien
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
@@ -20,6 +21,9 @@ def run_game():
     # Cria uma espaçonave
     ship = Ship(ai_settings, screen)
 
+    # Cria um alienígena
+    alien = Alien(ai_settings, screen)
+
     # Cria um grupo no qual serão armazenados os projéteis
     bullets: Group[Any] = Group()
 
@@ -28,7 +32,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
