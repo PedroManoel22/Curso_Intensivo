ceb4458f4981ae803b2a7a19a45790933061ebe6
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-05 14:21:58 -0300

    Importando os aliens

--- alien_ivasion/alien_invasion.py
+++ alien_ivasion/alien_invasion.py
@@ -2,7 +2,6 @@ from typing import Any
 
 import game_functions as gf
 import pygame
-from alien import Alien
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
@@ -21,18 +20,23 @@ def run_game():
     # Cria uma espaçonave
     ship = Ship(ai_settings, screen)
 
-    # Cria um alienígena
-    alien = Alien(ai_settings, screen)
+    # # Cria um alienígena
+    # alien = Alien(ai_settings, screen)
 
     # Cria um grupo no qual serão armazenados os projéteis
     bullets: Group[Any] = Group()
+
+    aliens: Group[Any] = Group()
+
+    # Cria a frota de alienígenas
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Inicia o laço principal do jogo
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
