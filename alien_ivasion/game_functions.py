92600e252bb2001867600847b742d848d3320299
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-12 09:48:34 -0300

    feat: implement fleet movement logic and edge response

--- alien_ivasion/game_functions.py
+++ alien_ivasion/game_functions.py
@@ -167,6 +167,24 @@ def create_fleet(
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
 
-def update_aliens(aliens: Group[Any]):
-    """Atualiza as posições de todos os alienígenas da frota."""
+def check_fleet_edges(ai_settings: Settings, aliens: Group[Any]):
+    """Responde apropriadamente se algum alienígena alcançou uma borda."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_diretion(ai_settings, aliens)
+            break
+
+
+def change_fleet_diretion(ai_settings: Settings, aliens: Group[Any]):
+    """Faz toda a frota descer e muda a sua direção."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+
+    ai_settings.fleet_direction *= -1
+
+
+def update_aliens(ai_settings: Settings, aliens: Group[Any]):
+    """Verifica se a frota está em uma das bordas
+    e então atualiza as posições de todos os alienígenas da frota."""
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
