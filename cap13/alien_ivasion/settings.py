be5238d54ce0b4ebeec82ee065d950796d1669c9
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-29 12:33:10 -0300

    Mudando a velocidade do alien e  porjétil e também mudando o tamanho da tela

--- cap13/alien_ivasion/settings.py
+++ cap13/alien_ivasion/settings.py
@@ -6,8 +6,8 @@ class Settings:
 
         # Configurações da tela
 
-        self.screen_widht = 1200
-        self.screen_height = 800
+        self.screen_widht = 640
+        self.screen_height = 360
 
         # Define a cor de fundo
         self.bg_color = (0, 0, 80)
@@ -21,15 +21,15 @@ class Settings:
         self.ship_speed_factor = 1
 
         # configurações dos projéteis
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 255, 165, 0
         self.bullets_allowed = 3
 
         # Configuração dos alienígenas
-        self.alien_speed_factor = 1
-        self.fleet_drop_speed = 10
+        self.alien_speed_factor = 0.5
+        self.fleet_drop_speed = 8
         # fleet_direction igual a 1 representa a direita; -1 representa
         # a esquerda
         self.fleet_direction = 1
