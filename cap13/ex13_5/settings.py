a9aca52d859b1c9e4b9f1ec2fa1281cc858a4fb7
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-29 16:55:18 -0300

    Configurações da tela e do personagem

---
+++ cap13/ex13_5/settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    def __init__(self):
+        # Configurações da tela
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        # Configurações do personagem
+        self.character_speed = 5.0
+
+        # Configurações da bola
+        self.ball_speed = 3.0
