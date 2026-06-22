0d701dade52654b695c5b2adcbd7fb147a360ba2
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-06-22 15:37:50 -0300

    Copia do módulo game_stats.py do projeto 1

---
+++ cap14/ex14_1/game_stats.py
@@ -0,0 +1,16 @@
+from settings import Settings
+
+
+class GameStats:
+    """Armazena dados estatísticos da Invasão Alienígena."""
+
+    def __init__(self, ai_settings: Settings) -> None:
+        """Inicializa os dados estatísticos."""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        # Inicializa a Invasão Alienígena em um estado ativo
+        self.game_active = False
+
+    def reset_stats(self):
+        """Inicializa os dados estatísticos que podem mudar durante o jogo."""
+        self.ships_left = self.ai_settings.ship_limit
