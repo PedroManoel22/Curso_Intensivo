a4e3ae6afc527ffe9f11e801810c18a1ec28b832
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-06-29 18:41:15 -0300

    Ajusta a escala de aceleração e as variáveis de velocidade dinâmica

--- cap14/ex14_3/settings.py
+++ cap14/ex14_3/settings.py
@@ -28,3 +28,23 @@ class Settings:
 
         # Limite de erros (tiros perdidos que passam da tela)
         self.misses_limit = 3
+
+        # A taxa com que a velocidade do jogo aumenta
+        self.speedup_scale = 1.1
+
+    def initialize_dynamic_settings(self) -> None:
+        """Inicializa as configurações que mudam no decorrer do jogo."""
+
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction igual a 1 representa a direita; -1 representa a esquerda.
+        self.fleet_direction = 1
+
+    def increse_speed(self) -> None:
+        """Aumenta as configurações de velocidade e o valor dos pontos."""
+
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
