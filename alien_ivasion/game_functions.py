4b2f746c06ff082750fed944eb173eb2f76e885c
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-03 10:59:39 -0300

    orrige contagem de pontos na colisao de alienigenas

--- alien_ivasion/game_functions.py
+++ alien_ivasion/game_functions.py
@@ -206,8 +206,9 @@ def check_bullet_alien_collisions(
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if collisions:
-        stats.score += ai_settings.alien_points
-        sb.prep_score()
+        for aliens in collisions.values():  # type: ignore
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
 
     if len(aliens) == 0:
         # Destroi os projéteis existentes e cria uma nova frota
