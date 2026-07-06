0abcd20a1952633c94f2f2a068e8915de310dabf
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-06 10:59:42 -0300

    Exibe naves restantes no placar

--- alien_ivasion/scoreboard.py
+++ alien_ivasion/scoreboard.py
@@ -1,6 +1,10 @@
+from typing import Any
+
 import pygame
 from game_stats import GameStats
+from pygame.sprite import Group
 from settings import Settings
+from ship import Ship
 
 
 class Scoreboard:
@@ -24,6 +28,7 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """Transforma a pontuação em uma imagem redenrizada."""
@@ -46,6 +51,9 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+
+        # Desenha as espaçonaves
+        self.ships.draw(self.screen)
 
     def prep_high_score(self):
         """Transforma a pontuação máxima em uma imagem renderizada."""
@@ -70,3 +78,12 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Mostra quantas espaçonaves restam."""
+        self.ships: Group[Any] = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
