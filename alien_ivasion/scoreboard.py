57ea642d234a2a7304a5651886b5dd46d94aee8c
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-06 10:20:24 -0300

    adicionar renderização de pontuação máxima e nível

--- alien_ivasion/scoreboard.py
+++ alien_ivasion/scoreboard.py
@@ -22,6 +22,8 @@ class Scoreboard:
 
         # Prepara a imagem da pontuação inicial
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Transforma a pontuação em uma imagem redenrizada."""
@@ -42,3 +44,29 @@ class Scoreboard:
         """Desenha a pontuação na tela."""
 
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+
+    def prep_high_score(self):
+        """Transforma a pontuação máxima em uma imagem renderizada."""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.ai_settings.bg_color
+        )
+
+        """Centraliza a pontuação máxima na parte superior da tela"""
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        """Transforma o nível em uma imagem renderizada."""
+        self.level_image = self.font.render(
+            str(self.stats.level), True, self.text_color, self.ai_settings.bg_color
+        )
+
+        # Posiciona o nível abaixo da pontuação
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
