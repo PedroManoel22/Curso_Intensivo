8814962be6d21b41602c1ef7340a09f6d5ee5110
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-06 16:15:23 -0300

    Copiando os arquivos do projeto principal para o ex 14_4

---
+++ cap14/ex14_4/game_stats.py
@@ -0,0 +1,21 @@
+from settings import Settings
+
+
+class GameStats:
+    """Armazena dados estatísticos da Invasão Alienígena."""
+
+    def __init__(self, ai_settings: Settings) -> None:
+        """Inicializa os dados estatísticos."""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        # Inicializa a Invasão Alienígena em um estado ativo
+        self.game_active = False
+
+        # A pontuação maxima jamais deverá ser reiniciada
+        self.high_score = 0
+
+    def reset_stats(self):
+        """Inicializa os dados estatísticos que podem mudar durante o jogo."""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
