6ae51c6bd4ca3de7c423baa10afe119f65550d6d
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-08 20:45:45 -0300

    Aplicando colormap ao gráfico

---
+++ cap15/ex15_2/main.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+
+# pyright: reportUnknownMemberType=false
+
+# Plotando os 5 primeiros números ao cubo
+
+numeros = list(range(1, 6))
+numeros_ao_cubo = [x**3 for x in numeros]
+plt.scatter(
+    numeros,
+    numeros_ao_cubo,
+    c=numeros_ao_cubo,
+    cmap=plt.cm.Blues,
+    edgecolors="none",
+    s=40,
+)
+
+# Define o título do gráfico e nomeia os eixos
+plt.title("Números ao cubo (5 primeiros)", fontsize=24)
+plt.xlabel("Valor", fontsize=14)
+plt.ylabel("Valor ao cubo", fontsize=14)
+
+plt.show()
+
+
+# Plotando os 5000 primeiros números ao cubo
+
+numeros = list(range(1, 5001))
+numeros_ao_cubo = [x**3 for x in numeros]
+plt.scatter(
+    numeros,
+    numeros_ao_cubo,
+    c=numeros_ao_cubo,
+    cmap=plt.cm.YlGn,
+    edgecolors="none",
+    s=40,
+)
+
+# Define o título do gráfico e nomeia os eixos
+plt.title("Números ao cubo (5000 primeiros)", fontsize=24)
+plt.xlabel("Valor", fontsize=14)
+plt.ylabel("Valor ao cubo", fontsize=14)
+
+plt.show()
