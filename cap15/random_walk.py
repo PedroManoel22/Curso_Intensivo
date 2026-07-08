f5b5706e9c04d6aae73954aa4c85080e95c8308e
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-08 20:52:06 -0300

    começando a classe

---
+++ cap15/random_walk.py
@@ -0,0 +1,10 @@
+class RandomWalk:
+    """Uma classe para gerar passeios aleatórios."""
+
+    def __init__(self, num_points: int = 5000) -> None:
+        """Inicia os atributos de um passeio."""
+        self.num_points = num_points
+
+        # Todos os passeios começam em (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
