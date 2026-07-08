ab82f7f92f7abb3e9c8f4114224be2530cde3455
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-08 20:46:20 -0300

    Aplicando colormap ao gráfico

--- cap15/scatter_squares.py
+++ cap15/scatter_squares.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
+
+diretorio_atual = Path(__file__).parent
+caminho_salvamento = diretorio_atual / "squares_plt.png"
 
 # pyright: reportUnknownMemberType=false
 
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, c=(0.84, 0.37, 0.00), edgecolors="none", s=40)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors="none", s=40)
 
 # Define o título do gráfico e nomeia os eixos
 
@@ -18,5 +23,11 @@ plt.axis([0, 1100, 0, 1100000])  # type: ignore
 
 # Define o tamanho dos rótulos das marcações
 plt.tick_params(axis="both", which="major", labelsize=14)
+
+# Salvando o gráfico em um arquivo png (Lembre-se de sempre salvar antes de exibir)
+plt.savefig(
+    caminho_salvamento, bbox_inches="tight"
+)  # segundo argumento remove espaços em branco ao redor do gráfico
+
 
 plt.show()
