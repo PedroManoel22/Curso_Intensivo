071bfd04f315af3c4aafe35bbfd4a59e45b3d6a8
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-24 10:05:47 -0300

    Resolução da segunda parte do exercicio 15_10

--- cap15/ex15_10/visualizacao_dados_pygal/rw_visual.py
+++ cap15/ex15_10/visualizacao_dados_pygal/rw_visual.py
@@ -1,5 +1,11 @@
-import matplotlib.pyplot as plt
+from pathlib import Path
+
+import pygal
 from random_walk import RandomWalk
+
+diretorio_atual = Path(__file__).parent
+print(diretorio_atual)
+nome_arquivo = diretorio_atual / "random_walk.svg"
 
 # pyright: reportUnknownMemberType=false
 
@@ -9,29 +15,15 @@ while True:
     rw = RandomWalk()
     rw.fill_walk()
 
-    # Define o tamanho da janela de plotagem
-    plt.figure(dpi=128, figsize=(10, 6))
+    visualizacao = pygal.Bar()
+    visualizacao.title = "Random Walk"
+    visualizacao.x_labels = [str(value) for value in range(rw.num_points)]
 
-    # Plota os pontos e mostra o gráfico
-    point_numbers = list(range(rw.num_points))
-    plt.scatter(
-        rw.x_values,
-        rw.y_values,
-        c=point_numbers,
-        cmap=plt.cm.Blues,
-        edgecolor="none",
-        s=1,
-    )
+    visualizacao.x_title = "Point Number"
+    visualizacao.y_title = "Distance from Origin"
+    visualizacao.add("Distance", rw.y_values)
 
-    # Enfatiza o primeiro e o último ponto
-    plt.scatter(0, 0, c="green", edgecolors="none", s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
-
-    # Remove os eixos
-    # plt.axes().get_xaxis().set_visible(False)
-    # plt.axes().get_yaxis().set_visible(False)
-
-    plt.show()
+    visualizacao.render_to_file(nome_arquivo)
 
     keep_running = input("Deseja gerar outro passeio aleatório? (s/n): ").strip()
     if keep_running.lower() == "n":
