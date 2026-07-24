cc9499252e2a8a3b5a70e27c2da339202f18647c
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-24 16:48:01 -0300

    adiciona dados de Death Valley e plota comparativo de temperaturas

--- teste/main.py
+++ teste/main.py
@@ -5,12 +5,13 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 
 # Obtém as datas e as temperaturas máximas do arquivo
-root_dir = Path(__file__).parent.parent
-
-print(root_dir)
+root_dir = Path(__file__).parent
 
 filename_san_francisco = root_dir / "san_francisco_weather_2014.csv"
+filename_death = root_dir / "death_valley_2014.csv"
 
+
+# ler arquivo san francisco
 with open(filename_san_francisco) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -37,13 +38,47 @@ with open(filename_san_francisco) as f:
             highs.append(high)
             lows.append(low)
 
+
+# ler arquivo death
+with open(filename_death) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Pegando as temperaturas máxiams de cada dia
+    dates_death: list[datetime] = []
+    highs_death: list[int] = []
+    lows_death: list[int] = []
+
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d").replace(
+                tzinfo=timezone.utc
+            )
+
+            high = int(row[1])
+            low = int(row[3])
+
+        except ValueError:
+            print(current_date, "missing data")  # type: ignore[reportUnknownMemberType]
+
+        else:
+            dates_death.append(current_date)
+            highs_death.append(high)
+            lows_death.append(low)
+
 fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
 
 ax.plot(dates, highs, c="red", alpha=0.5)  # type: ignore[reportUnknownMemberType]
 ax.plot(dates, lows, c="blue", alpha=0.5)  # type: ignore[reportUnknownMemberType]
 ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)  # type: ignore[reportUnknownMemberType]
 
-ax.set_title("Daily high and low temperatures - 2014\nSan Fracisco", fontsize=20)  # type: ignore[reportUnknownMemberType]
+ax.plot(dates_death, highs_death, c="yellow", alpha=0.5)  # type: ignore[reportUnknownMemberType]
+ax.plot(dates_death, lows_death, c="black", alpha=0.5)  # type: ignore[reportUnknownMemberType]
+ax.fill_between(dates_death, highs_death, lows_death, facecolor="blue", alpha=0.1)  # type: ignore[reportUnknownMemberType]
+
+ax.set_title(
+    "Daily high and low temperatures - 2014\nSan Fracisco / Death Valley", fontsize=20
+)  # type: ignore[reportUnknownMemberType]
 ax.set_xlabel("", fontsize=1)  # type: ignore[reportUnknownMemberType]
 ax.set_ylabel("Temperatura (F)", fontsize=16)  # type: ignore[reportUnknownMemberType]
 ax.tick_params(axis="both", which="major", labelsize=16)  # type: ignore[reportUnknownMemberType]
