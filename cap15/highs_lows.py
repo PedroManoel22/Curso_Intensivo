937987c4374a6945ce3dbdc591ae92e13baef159
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-24 16:00:14 -0300

    pegando os dados meteorológicos de San Francisco em 2014

--- cap15/highs_lows.py
+++ cap15/highs_lows.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 # Obtém as datas e as temperaturas máximas do arquivo
 root_dir = Path(__file__).parent
-filename = root_dir / "sitka_weather_2021_full.csv"
+filename = root_dir / "death_valley_2014.csv"
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -19,15 +19,15 @@ with open(filename) as f:
 
     for row in reader:
         try:
-            current_date = datetime.strptime(row[2], "%Y-%m-%d").replace(
+            current_date = datetime.strptime(row[0], "%Y-%m-%d").replace(
                 tzinfo=timezone.utc
             )
 
-            high = int(row[7])
-            low = int(row[8])
+            high = int(row[1])
+            low = int(row[3])
 
         except ValueError:
-            pass
+            print(current_date, "missing data")
 
         else:
             dates.append(current_date)
@@ -41,9 +41,7 @@ plt.fill_between(
     dates, highs, lows, facecolor="blue", alpha=0.1
 )  # Preenche a área entre as linhas
 
-plt.title(
-    "Temperaturas Máximas e Mínimas Diárias, 10 primeiros dias - 2021", fontsize=20
-)
+plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize=20)
 plt.xlabel("", fontsize=1)
 fig.autofmt_xdate()  # Formata as datas para não sobrepor
 plt.ylabel("Temperatura (F)", fontsize=16)
