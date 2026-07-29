7c3a24db65db71d69deec0c617f6bda79c066981
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-29 11:58:06 -0300

    Copia do coutry_codes.pt para a pasta ex16_5

---
+++ cap16/ex16_5/country_codes.py
@@ -0,0 +1,13 @@
+from pygal_maps_world.i18n import COUNTRIES
+
+
+def get_country_code(country_name: str):
+    """Devolve o código de duas letras do Pygal para um país, dado o seu nome."""
+
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+        else:
+            ...
+
+    return None
