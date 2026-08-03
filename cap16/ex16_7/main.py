6402c15eea43d3235d84c4b016cf43bfd052b0a7
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-08-03 09:49:03 -0300

    gera mapa mundial de gastos governamentais em educação

--- cap16/ex16_7/main.py
+++ cap16/ex16_7/main.py
@@ -1,4 +1,16 @@
-from pathlib import Path
+import pygal_maps_world.maps
+from process_data import ROOT_DIR, get_code_expenses
+from pygal.style import LightColorizedStyle as LCS
+from pygal.style import RotateStyle as RS
 
-root_dir = Path(__file__).parent
-file_name = "dados.csv"
+FILE_NAME = "resultado.svg"
+ROOT_FILE = ROOT_DIR / FILE_NAME
+
+datas = get_code_expenses()
+
+wm_style = RS("#BA55D3", base_style=LCS)
+wm = pygal_maps_world.maps.World(style=wm_style)
+wm.title = "Government expenditure on education, total (% of GDP) - 2021"
+wm.add("Spending on Education (% of GDP)", datas)  # type: ignore
+
+wm.render_to_file(ROOT_FILE)  # type: ignore
