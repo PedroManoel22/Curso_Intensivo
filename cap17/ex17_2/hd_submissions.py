8bf3757a1e703495c3cd16f89344d9f61420a653
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-08-09 15:33:15 -0300

    Busca e ordena top discussões do Hacker News por comentários

--- cap17/ex17_2/hd_submissions.py
+++ cap17/ex17_2/hd_submissions.py
@@ -8,36 +8,44 @@ url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 r = requests.get(url)
 print(f"Status code: {r.status_code}", "-> Top stories retrieved successfully\n")
 
-# Processa informações sobre cada artigo submetido
-submission_ids = r.json()
-submissions_dicts: list[dict[str, Any]] = []
 
-for submission_id in submission_ids[:30]:
-    # Cria uma chamada de API separada para cada artigo submetido
-    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
-    submission_r = requests.get(url)
-    response_dict = submission_r.json()
+def get_top_submissions() -> list[dict[str, Any]]:
+    # Processa informações sobre cada artigo submetido
+    submission_ids = r.json()
+    submissions_dicts: list[dict[str, Any]] = []
 
-    print(
-        "Status code:",
-        submission_r.status_code,
-        "-> Title: ",
-        response_dict["title"],
-        "\n",
-    )
+    for submission_id in submission_ids[:30]:
+        # Cria uma chamada de API separada para cada artigo submetido
+        url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+        submission_r = requests.get(url)
+        response_dict = submission_r.json()
 
-    submission_dict: dict[str, Any] = {
-        "title": response_dict["title"],
-        "link": "http://news.ycombinator.com/item?id=" + str(submission_id),
-        "comments": response_dict.get("descendants", 0),
-    }
-    submissions_dicts.append(submission_dict)
+        print(
+            "Status code:",
+            submission_r.status_code,
+            "-> Title: ",
+            response_dict["title"],
+            "\n",
+        )
 
-    submissions_dicts = sorted(
-        submissions_dicts, key=itemgetter("comments"), reverse=True
-    )
+        submission_dict: dict[str, Any] = {
+            "title": response_dict["title"],
+            "xlink": "http://news.ycombinator.com/item?id=" + str(submission_id),
+            "label": int(response_dict.get("descendants", 0)),
+        }
+        submissions_dicts.append(submission_dict)
 
-for submission_dict in submissions_dicts:
-    print("\nTitle:", submission_dict["title"])
-    print("Discussion link:", submission_dict["link"])
-    print("Comments:", submission_dict["comments"])
+        submissions_dicts = sorted(
+            submissions_dicts, key=itemgetter("label"), reverse=True
+        )
+
+    for submission_dict in submissions_dicts:
+        print("\nTitle:", submission_dict["title"])
+        print("Discussion link:", submission_dict["xlink"])
+        print("Comments:", submission_dict["label"])
+
+    return submissions_dicts
+
+
+if __name__ == "__main__":
+    submissions_dicts = get_top_submissions()
